3879a058c83ec227511df0405894f4421f4e9f1c
Michael Barton <mail@michaelbarton.me.uk>
2010-02-16 16:17:35 -0500

    Shell script to automate alignment

---
+++ run.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+if [ -d tmp ]; then
+  rm -rf tmp
+fi
+
+mkdir tmp
+cd ./tmp/
+  cat ../src_data/rRNA/*.fasta > rRNA.fasta
+  cp ../src_data/alignment/green_genes.align .
+  mothur "#align.seqs(candidate=rRNA.fasta, template=green_genes.align)"
+  mothur "#filter.seqs(fasta=rRNA.align,vertical=T)"
+cd ..
+
+if [ -d alignment ]; then
+  rm -rf alignment
+fi
+mkdir alignment
+
+cd ./alignment/
+  cp ../tmp/rRNA.filter.fasta rRNA.align
+cd ..
+
+rm -rf tmp
